e6ec34685785abbaf5b593bddeac9cc39aa8c0be
Harsha <konndapuramharsha@gmail.com>
2021-02-09 16:04:54 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:practice/Pages/home_page.dart';
+import 'package:practice/Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text(
-              "Hi I am Harsha",
-            ),
-          ),
-        ),
+      // home: HomePage(),
+      themeMode:ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
       ),
+      initialRoute: "/home",
+      routes: {
+         "/" :(context) => LoginPage(),
+         "/home":(context) => HomePage(),
+         "/login": (context) => LoginPage(),
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
