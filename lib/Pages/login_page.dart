f71e638f9638ac8cb0a5e7e453db0ab9b1f55976
Harsha <konndapuramharsha@gmail.com>
2021-02-09 21:43:54 +0530

    Day 5 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,56 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:practice/utils/routes.dart';
 class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(children: [
-        SizedBox(
-          height: 20.0,
-        ),
-        Image.asset("assets/images/login_img.png",height: 270,),
-        SizedBox(
-          height: 5.0,
-        ),
-        Text("Welcome",style: TextStyle(
-          fontSize: 24,
-          color: Colors.black87,
-          fontWeight: FontWeight.bold,
-        ),),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal:32.0 ),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Enter User Name",
-                  labelText:"User Name" 
-                ),
-              ),
-               TextFormField(
-                 obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Enter Password",
-                  labelText:"Password" 
-                ),
-              ),
-              SizedBox(
-          height: 20.0,
-        ),
-        ElevatedButton(
-          child: Text("Login"),
-          style: TextButton.styleFrom(),
-          onPressed: (){
-            print("Hi Harsha");
-          },
-        ),
-
-            ],
+      child: SingleChildScrollView(
+              child: Column(children: [
+          SizedBox(
+            height: 5.0,
           ),
-        )
+          Image.asset("assets/images/login_img.png",height: 260,),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text("Welcome",style: TextStyle(
+            fontSize: 24,
+            color: Colors.black87,
+            fontWeight: FontWeight.bold,
+          ),),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal:32.0 ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter User Name",
+                    labelText:"User Name" 
+                  ),
+                ),
+                 TextFormField(
+                   obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText:"Password" 
+                  ),
+                ),
+                SizedBox( 
+            height: 20.0,
+            
+          ),
+          ElevatedButton(
+            child: Text("Login"),
+            style: TextButton.styleFrom(minimumSize:
+             Size(150,40),
+            ),
+            onPressed: (){
+              Navigator.pushNamed(context, MyRoutes.homeRoute);
+            },
+          ),
+              ],
+            ),
+          )
 
-      ],)
+        ],),
+      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:practice/Pages/home_page.dart';
 import 'package:practice/Pages/login_page.dart';
+import 'package:practice/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,14 +17,14 @@ class MyApp extends StatelessWidget {
       // home: HomePage(),
       themeMode:ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.notoSans().fontFamily,
       ),
       initialRoute: "/login",
       routes: {
          "/" :(context) => LoginPage(),
-         "/home":(context) => HomePage(),
-         "/login": (context) => LoginPage(),
+         MyRoutes.homeRoute:(context) => HomePage(),
+         MyRoutes.loginRoute: (context) => LoginPage(),
       },
       debugShowCheckedModeBanner: false,
     );
